2b7dfeb1dab6f75fd44f4a3ad434778b6e16a964
David Candelaria <davidcandelaria@Davids-MacBook-Pro.local>
2026-06-02 12:03:44 -0600

    feature: create new dashboard route - define boilerplate template structure

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+    return(
+        <div>
+            <h1 className="text-2xl font-bold text-[#2D4A3E]">Welcome to your Dashboard</h1>
+        </div>
+    )
+}
