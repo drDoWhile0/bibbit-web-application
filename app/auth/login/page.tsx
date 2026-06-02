d292a1c9460b2d3303a5a6cfbe8724f68cf8bd64
David Candelaria <davidcandelaria@Davids-MacBook-Pro.local>
2026-06-01 22:53:30 -0600

    feature: add new login page - renders the Authlayout and loginform components

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from '@/components/auth/AuthLayout'
+import LoginForm from '@/components/auth/LoginForm'
+
+export default function LogInPage() {
+    return (
+        <AuthLayout>
+            <LoginForm />
+        </AuthLayout>
+    )
+}
